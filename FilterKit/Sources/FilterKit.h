201abddbf886feb6d977bb21e623ec9df55422ad
Jonas Olsson <jonas.olsson@finn.no>
2018-04-24 13:22:47 +0200

    Restructure folders

---
+++ FilterKit/Sources/FilterKit.h
@@ -0,0 +1,11 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for FilterKit.
+FOUNDATION_EXPORT double FilterKitVersionNumber;
+
+//! Project version string for FilterKit.
+FOUNDATION_EXPORT const unsigned char FilterKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FilterKit/PublicHeader.h>
+
+

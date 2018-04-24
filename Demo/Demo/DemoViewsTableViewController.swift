3b41b0001c393e7aa59e425cb53019f5641ec66d
Jonas Olsson <jonas.olsson@finn.no>
2018-04-24 11:18:45 +0200

    Added some initial setup for demo app

---
+++ Demo/Demo/DemoViewsTableViewController.swift
@@ -0,0 +1,99 @@
+//
+//  Copyright © FINN.no AS, Inc. All rights reserved.
+//
+
+import FilterKit
+import UIKit
+
+// MARK: - DemoViewsTableViewController
+
+class DemoViewsTableViewController: UITableViewController {
+    init() {
+        super.init(style: .grouped)
+    }
+
+    required init?(coder aDecoder: NSCoder) { fatalError("") }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        if let indexPath = Sections.lastSelectedIndexPath {
+            let viewController = Sections.viewController(for: indexPath)
+            presentViewControllerWithDismissGesture(viewController)
+        }
+    }
+
+    private func setup() {
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.backgroundColor = UIColor.white
+        tableView.delegate = self
+        tableView.separatorStyle = .none
+    }
+}
+
+// MARK: - UITableViewDelegate
+
+extension DemoViewsTableViewController {
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return Sections.all.count
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let section = Sections.all[section]
+        return section.numberOfItems
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = Sections.formattedName(for: indexPath)
+        cell.selectionStyle = .none
+        cell.backgroundColor = .clear
+
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        Sections.lastSelectedIndexPath = indexPath
+        let viewController = Sections.viewController(for: indexPath)
+        presentViewControllerWithDismissGesture(viewController)
+    }
+
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 50
+    }
+
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return Sections.formattedName(for: section)
+    }
+}
+
+extension String {
+    var capitalizingFirstLetter: String {
+        return prefix(1).uppercased() + dropFirst()
+    }
+}
+
+extension DemoViewsTableViewController {
+    func presentViewControllerWithDismissGesture(_ viewController: UIViewController) {
+        self.present(viewController, animated: true) {
+            let dismissGesture = UITapGestureRecognizer(target: self, action: #selector(self.closeCurrentlyPresentedViewController))
+            dismissGesture.numberOfTapsRequired = 2
+            viewController.view.addGestureRecognizer(dismissGesture)
+        }
+    }
+
+    @objc func closeCurrentlyPresentedViewController() {
+        Sections.lastSelectedIndexPath = nil
+        dismiss(animated: true, completion: nil)
+    }
+}
--- Demo/Demo/ViewController.swift
+++ Demo/Demo/ViewController.swift
@@ -4,14 +4,15 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = .white
+        let label = UILabel(frame: view.bounds)
+        label.text = "Hello World"
+        view.addSubview(label)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 

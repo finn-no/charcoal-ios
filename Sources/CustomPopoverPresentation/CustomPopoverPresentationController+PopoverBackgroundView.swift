1cfef53a68334ec328b2eade1d9e8015ad556b11
Henrik Holmsen <henrik.holmsen@gmail.com>
2018-05-03 12:01:50 +0200

    Add custom popover presentation (#7)
    * Add horisontal scroll button group with popover demo
    
    * Add a custom UIPopoverPresentationController
    
    * Use default iOS insets for popovers and use an image asset for the popover arrow
    
    * Add a CustomPopoverPresentationTransitioningDelegate object
    
    * Remove attribution headers
    
    * Use Bundle extension form FilterKit.swift for the popoverArrowImage
    
    * Remove redundant UIFont prefix for UIFont.body
    
    * Fix typo
    
    * Rename Filter model to PreferenceFilter and its subfilters property to values
    
    * Use default iOS UIPopoverView corner radius
    
    * Refactor handling of UIDimmingView and add comments
    
    * Run SwiftFormat
    
    * Use optional chaining instead of unwrapping optional by `if let` statement

---
+++ Sources/CustomPopoverPresentation/CustomPopoverPresentationController+PopoverBackgroundView.swift
@@ -0,0 +1,63 @@
+//
+//  Copyright © FINN.no AS, Inc. All rights reserved.
+//
+
+import UIKit
+
+extension CustomPopoverPresentationController {
+    class PopoverBackgroundView: UIPopoverBackgroundView {
+        var arrowView: UIImageView?
+
+        private var _arrowOffset: CGFloat = 0.0
+        override var arrowOffset: CGFloat {
+            get { return _arrowOffset }
+            set {
+                _arrowOffset = newValue
+                setNeedsLayout()
+            }
+        }
+
+        static let popoverArrowImage: UIImage? = {
+            return UIImage(named: "popoverArrow", in: .filterKit, compatibleWith: nil)
+        }()
+
+        override var arrowDirection: UIPopoverArrowDirection {
+            get { return .up }
+            set { fatalError("Setting arrowDirection is not available for \(type(of: self))") }
+        }
+
+        override class var wantsDefaultContentAppearance: Bool {
+            return false
+        }
+
+        override static func contentViewInsets() -> UIEdgeInsets {
+            return UIEdgeInsets(top: .smallSpacing, left: 0, bottom: 0, right: 0)
+        }
+
+        override static func arrowBase() -> CGFloat {
+            return popoverArrowImage?.size.width ?? 0.0
+        }
+
+        override static func arrowHeight() -> CGFloat {
+            return popoverArrowImage?.size.height ?? 0.0
+        }
+
+        override func layoutSubviews() {
+            super.layoutSubviews()
+
+            if arrowView == nil {
+                let width = PopoverBackgroundView.arrowBase()
+                let height = PopoverBackgroundView.arrowHeight()
+                let x = ((self.frame.size.width / 2) + arrowOffset) - (width / 2)
+                let y = PopoverBackgroundView.contentViewInsets().top
+                let frame = CGRect(x: x, y: y, width: width, height: height)
+
+                let arrowView = UIImageView(frame: frame)
+                arrowView.image = PopoverBackgroundView.popoverArrowImage
+                addSubview(arrowView)
+
+                self.arrowView = arrowView
+            }
+        }
+    }
+}

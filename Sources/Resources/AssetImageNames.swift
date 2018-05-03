031b6d5e3dc87551d58f697ef14cbc08fcee9ba7
Holmsen, Henrik <henrik.holmsen@schibsted.com>
2018-05-03 13:58:29 +0200

    Build updated script

--- Sources/Resources/AssetImageNames.swift
+++ Sources/Resources/AssetImageNames.swift
@@ -4,8 +4,8 @@
 import UIKit
 
 extension UIImage {
-    convenience init(named imageAsset: ImageAsset) {
-        self.init(named: imageAsset.rawValue)!
+    convenience init(named imageAsset: ImageAsset, in bundle: Bundle? = nil, compatibleWith traitCollection: UITraitCollection? = nil) {
+        self.init(named: imageAsset.rawValue, in: bundle, compatibleWith: traitCollection)!
     }
 }
 

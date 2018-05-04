3b3735ad31a29595430e104463376262f3f79c40
Henrik Holmsen <henrik.holmsen@gmail.com>
2018-05-04 14:54:52 +0200

    Fix dimming view on popover (#12)
    * Add extensions for layout
    
    * Add our own dimming view instead of changing Apples
    
    * Use UIEdgeInsets instead of Insets
    
    * Reset the bundle identifier after unintentional commit
    
    * Make UIEdgeInsets extension unavailable outside FilterKit module

--- Sources/CustomPopoverPresentation/CustomPopoverPresentationController.swift
+++ Sources/CustomPopoverPresentation/CustomPopoverPresentationController.swift
@@ -19,6 +19,18 @@ final class CustomPopoverPresentationController: UIPopoverPresentationController
         return .up
     }
 
+    public override var permittedArrowDirections: UIPopoverArrowDirection {
+        get { return .up }
+        set { fatalError("Only UIPopoverArrowDirection.up is currently permitted for \(String(describing: type(of: self))).") }
+    }
+
+    private lazy var dimmingView: UIView = {
+        let view = UIView(frame: .zero)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = UIColor.black.withAlphaComponent(0.4)
+        return view
+    }()
+
     private var _popoverLayouMargins: UIEdgeInsets?
     public override var popoverLayoutMargins: UIEdgeInsets {
         get {
@@ -43,21 +55,24 @@ final class CustomPopoverPresentationController: UIPopoverPresentationController
             return
         }
 
-        // The alpha for the UIDimmingView provided by Apple is 20%. According to design we need 40%
-        dimmingView?.backgroundColor = UIColor.black.withAlphaComponent(.dimmingViewAlpha)
-
         snapshotView.frame = sourceView.convert(sourceView.bounds, to: containerView)
         snapshotView.alpha = 0.0
         containerView?.addSubview(snapshotView)
         self.snapshotView = snapshotView
 
-        presentingViewController.transitionCoordinator?.animate(alongsideTransition: { _ in
+        dimmingView.alpha = 0.0
+        containerView?.insertSubview(dimmingView, at: 0)
+        dimmingView.fillInSuperview()
+
+        presentingViewController.transitionCoordinator?.animate(alongsideTransition: { [weak self] _ in
+            self?.dimmingView.alpha = 1.0
             snapshotView.alpha = 1.0
         })
     }
 
     public override func presentationTransitionDidEnd(_ completed: Bool) {
         if completed == false {
+            dimmingView.removeFromSuperview()
             snapshotView?.removeFromSuperview()
         }
 
@@ -71,33 +86,19 @@ final class CustomPopoverPresentationController: UIPopoverPresentationController
 
         presentingViewController.transitionCoordinator?.animate(alongsideTransition: { [weak self] _ in
             self?.snapshotView?.alpha = 0.0
+            self?.dimmingView.alpha = 0.0
+            self?.presentedViewController.view.alpha = 0.0
         })
     }
 
     public override func dismissalTransitionDidEnd(_ completed: Bool) {
         if completed {
+            dimmingView.removeFromSuperview()
             snapshotView?.removeFromSuperview()
         }
 
         super.dismissalTransitionDidEnd(completed)
     }
-}
-
-private extension CustomPopoverPresentationController {
-    var dimmingView: UIView? {
-        // The UIDimmingView class is not publicly available so our only option is to find it by its class name.
-        return containerView?.subviews.filter({ $0.isView(named: "UIDimmingView") }).first
-    }
-}
-
-private extension UIView {
-    func isView(named name: String) -> Bool {
-        return String(describing: type(of: self)) == name
-    }
-}
-
-private extension CGFloat {
-    static let dimmingViewAlpha: CGFloat = 0.4
 }
 
 private extension UIEdgeInsets {

40aa1ee6478404f766968ff47cea4072e2ea738c
Holmsen, Henrik <henrik.holmsen@schibsted.com>
2018-05-04 11:30:18 +0200

    Make UIEdgeInsets extension unavailable outside FilterKit module

--- Sources/Extensions/UIKit/UIEdgeInsetsExtensions.swift
+++ Sources/Extensions/UIKit/UIEdgeInsetsExtensions.swift
@@ -4,7 +4,7 @@
 
 import UIKit
 
-public extension UIEdgeInsets {
+extension UIEdgeInsets {
     init(top: CGFloat = 0, leading: CGFloat = 0, bottom: CGFloat = 0, trailing: CGFloat = 0) {
         self.init(top: top, left: leading, bottom: bottom, right: trailing)
     }

4b49e2f91829a71454a8dd1f867f99c05b2c2976
Jonas Olsson <osanoj@users.noreply.github.com>
2018-05-25 10:00:18 +0200

    Additional hockeyapp setup (#25)

--- Demo/AppDelegate.swift
+++ Demo/AppDelegate.swift
@@ -11,8 +11,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         BITHockeyManager.shared().configure(withIdentifier: "dcb11108644344b5ae22b778ed0fcf9d")
+        BITHockeyManager.shared().crashManager.crashManagerStatus = .autoSend
+        BITHockeyManager.shared().logLevel = .warning
         BITHockeyManager.shared().isFeedbackManagerDisabled = true
+        BITHockeyManager.shared().authenticator.identificationType = .anonymous
         BITHockeyManager.shared().start()
+        BITHockeyManager.shared().authenticator.authenticateInstallation()
 
         window = UIWindow(frame: UIScreen.main.bounds)
         let viewController = DemoViewsTableViewController()

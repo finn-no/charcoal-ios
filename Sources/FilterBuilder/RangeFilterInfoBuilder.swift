0895795268219036c95b1acb5f550cd4d82e6ee0
Holmsen, Henrik <henrik.holmsen@schibsted.com>
2018-06-26 10:22:08 +0200

    Apply changes in RangeFilterView to RangeFilterInfoType

--- Sources/FilterBuilder/RangeFilterInfoBuilder.swift
+++ Sources/FilterBuilder/RangeFilterInfoBuilder.swift
@@ -29,108 +29,216 @@ public final class RangeFilterInfoBuilder {
 
 private extension RangeFilterInfoBuilder {
     func buildRangeFilterInfoForCarMarket(from filterData: FilterData) -> RangeFilterInfoType? {
-        let title = filterData.title
+        let name = filterData.title
         let lowValue: Int
         let highValue: Int
         let steps: Int
         let unit: String
+        let rangeBoundsOffsets: RangeFilterInfo.RangeBoundsOffsets
+        let referenceValues: RangeFilterInfo.ReferenceValues
+        let accessibilityValues: RangeFilterInfo.AccessibilityValues
+        let appearanceProperties: RangeFilterInfo.AppearenceProperties
 
         switch filterData.key {
         case .year:
             lowValue = 1950
-            let currentYear = Calendar.current.component(.year, from: Date())
-            highValue = currentYear
-            steps = highValue - lowValue
+            highValue = Calendar.current.component(.year, from: Date())
             unit = "år"
+            rangeBoundsOffsets = (10, 10)
+            referenceValues = defaultReferencValuesForRange(withLowValue: lowValue, andHighValue: highValue)
+            steps = calculatedStepsForRange(withLowValue: lowValue, highValue: highValue, rangeBoundsOffsets: rangeBoundsOffsets, incrementedBy: 1)
+            accessibilityValues = (accessibilitySteps: nil, accessibilityValueSuffix: nil)
+            appearanceProperties = (usesSmallNumberInputFont: false, displaysUnitInNumberInput: false, isCurrencyValueRange: false)
         case .engineEffect:
             lowValue = 0
             highValue = 500
-            steps = 100
             unit = "hk"
+            rangeBoundsOffsets = (0, 10)
+            referenceValues = defaultReferencValuesForRange(withLowValue: lowValue, andHighValue: highValue)
+            steps = calculatedStepsForRange(withLowValue: lowValue, highValue: highValue, rangeBoundsOffsets: rangeBoundsOffsets, incrementedBy: 10)
+            accessibilityValues = (accessibilitySteps: nil, accessibilityValueSuffix: nil)
+            appearanceProperties = (usesSmallNumberInputFont: false, displaysUnitInNumberInput: true, isCurrencyValueRange: false)
         case .mileage:
             lowValue = 0
             highValue = 200_000
-            steps = 200
             unit = "km"
+            rangeBoundsOffsets = (0, 10)
+            referenceValues = defaultReferencValuesForRange(withLowValue: lowValue, andHighValue: highValue)
+            steps = calculatedStepsForRange(withLowValue: lowValue, highValue: highValue, rangeBoundsOffsets: rangeBoundsOffsets, incrementedBy: 1000)
+            accessibilityValues = (accessibilitySteps: nil, accessibilityValueSuffix: nil)
+            appearanceProperties = (usesSmallNumberInputFont: false, displaysUnitInNumberInput: true, isCurrencyValueRange: false)
         case .numberOfSeats:
             lowValue = 0
             highValue = 10
-            steps = 10
             unit = "seter"
+            rangeBoundsOffsets = (0, 1)
+            referenceValues = defaultReferencValuesForRange(withLowValue: lowValue, andHighValue: highValue)
+            steps = calculatedStepsForRange(withLowValue: lowValue, highValue: highValue, rangeBoundsOffsets: rangeBoundsOffsets, incrementedBy: 1)
+            accessibilityValues = (accessibilitySteps: nil, accessibilityValueSuffix: nil)
+            appearanceProperties = (usesSmallNumberInputFont: false, displaysUnitInNumberInput: true, isCurrencyValueRange: false)
         case .price:
             lowValue = 0
             highValue = 500_000
-            steps = 500
             unit = "kr"
+            rangeBoundsOffsets = (0, 1000)
+            referenceValues = defaultReferencValuesForRange(withLowValue: lowValue, andHighValue: highValue)
+            steps = calculatedStepsForRange(withLowValue: lowValue, highValue: highValue, rangeBoundsOffsets: rangeBoundsOffsets, incrementedBy: 1000)
+            accessibilityValues = (accessibilitySteps: nil, accessibilityValueSuffix: nil)
+            appearanceProperties = (usesSmallNumberInputFont: false, displaysUnitInNumberInput: true, isCurrencyValueRange: true)
         default:
             return nil
         }
 
-        return RangeFilterInfo(name: title, lowValue: lowValue, highValue: highValue, steps: steps, unit: unit)
+        return RangeFilterInfo(
+            name: name,
+            lowValue: lowValue,
+            highValue: highValue,
+            steps: steps,
+            rangeBoundsOffsets: rangeBoundsOffsets,
+            unit: unit,
+            referenceValues: referenceValues,
+            accesibilityValues: accessibilityValues,
+            appearanceProperties: appearanceProperties
+        )
     }
 
     func buildRangeFilterInfoForRealestateMarket(from filterData: FilterData) -> RangeFilterInfoType? {
-        let title = filterData.title
+        let name = filterData.title
         let lowValue: Int
         let highValue: Int
         let steps: Int
         let unit: String
+        let rangeBoundsOffsets: RangeFilterInfo.RangeBoundsOffsets
+        let referenceValues: RangeFilterInfo.ReferenceValues
+        let accessibilityValues: RangeFilterInfo.AccessibilityValues
+        let appearanceProperties: RangeFilterInfo.AppearenceProperties
 
         switch filterData.key {
         case .price, .priceCollective:
             lowValue = 0
             highValue = 10_000_000
-            steps = 100
+            rangeBoundsOffsets = (0, 100_000)
             unit = "kr"
+            referenceValues = defaultReferencValuesForRange(withLowValue: lowValue, andHighValue: highValue)
+            steps = calculatedStepsForRange(withLowValue: lowValue, highValue: highValue, rangeBoundsOffsets: rangeBoundsOffsets, incrementedBy: 10000)
+            accessibilityValues = (accessibilitySteps: nil, accessibilityValueSuffix: nil)
+            appearanceProperties = (usesSmallNumberInputFont: true, displaysUnitInNumberInput: false, isCurrencyValueRange: true)
         case .rent:
             lowValue = 0
             highValue = 20000
-            steps = 200
+            rangeBoundsOffsets = (0, 1000)
             unit = "kr"
+            referenceValues = defaultReferencValuesForRange(withLowValue: lowValue, andHighValue: highValue)
+            steps = calculatedStepsForRange(withLowValue: lowValue, highValue: highValue, rangeBoundsOffsets: rangeBoundsOffsets, incrementedBy: 100)
+            accessibilityValues = (accessibilitySteps: nil, accessibilityValueSuffix: nil)
+            appearanceProperties = (usesSmallNumberInputFont: false, displaysUnitInNumberInput: true, isCurrencyValueRange: true)
         case .noOfBedrooms:
             lowValue = 0
             highValue = 6
-            steps = 6
-            unit = ""
+            rangeBoundsOffsets = (1, 1)
+            unit = "soverom"
+            referenceValues = defaultReferencValuesForRange(withLowValue: lowValue, andHighValue: highValue)
+            steps = calculatedStepsForRange(withLowValue: lowValue, highValue: highValue, rangeBoundsOffsets: rangeBoundsOffsets, incrementedBy: 1)
+            accessibilityValues = (accessibilitySteps: nil, accessibilityValueSuffix: nil)
+            appearanceProperties = (usesSmallNumberInputFont: true, displaysUnitInNumberInput: true, isCurrencyValueRange: false)
         case .area:
             lowValue = 0
             highValue = 400
-            steps = 80
+            rangeBoundsOffsets = (0, 100)
             unit = "m\u{00B2}"
+            referenceValues = defaultReferencValuesForRange(withLowValue: lowValue, andHighValue: highValue)
+            steps = calculatedStepsForRange(withLowValue: lowValue, highValue: highValue, rangeBoundsOffsets: rangeBoundsOffsets, incrementedBy: 1)
+            accessibilityValues = (accessibilitySteps: nil, accessibilityValueSuffix: nil)
+            appearanceProperties = (usesSmallNumberInputFont: true, displaysUnitInNumberInput: true, isCurrencyValueRange: false)
         case .plotArea:
             lowValue = 0
             highValue = 6000
-            steps = 80
+            rangeBoundsOffsets = (0, 1000)
             unit = "m\u{00B2}"
+            referenceValues = defaultReferencValuesForRange(withLowValue: lowValue, andHighValue: highValue)
+            steps = calculatedStepsForRange(withLowValue: lowValue, highValue: highValue, rangeBoundsOffsets: rangeBoundsOffsets, incrementedBy: 10)
+            accessibilityValues = (accessibilitySteps: nil, accessibilityValueSuffix: nil)
+            appearanceProperties = (usesSmallNumberInputFont: false, displaysUnitInNumberInput: true, isCurrencyValueRange: false)
         case .constructionYear:
             lowValue = 1900
-            highValue = 2018
-            steps = highValue - lowValue
+            highValue = Calendar.current.component(.year, from: Date())
+            rangeBoundsOffsets = (10, 10)
             unit = ""
+            referenceValues = defaultReferencValuesForRange(withLowValue: lowValue, andHighValue: highValue)
+            steps = calculatedStepsForRange(withLowValue: lowValue, highValue: highValue, rangeBoundsOffsets: rangeBoundsOffsets, incrementedBy: 1)
+            accessibilityValues = (accessibilitySteps: nil, accessibilityValueSuffix: nil)
+            appearanceProperties = (usesSmallNumberInputFont: false, displaysUnitInNumberInput: false, isCurrencyValueRange: false)
         default:
             return nil
         }
 
-        return RangeFilterInfo(name: title, lowValue: lowValue, highValue: highValue, steps: steps, unit: unit)
+        return RangeFilterInfo(
+            name: name,
+            lowValue: lowValue,
+            highValue: highValue,
+            steps: steps,
+            rangeBoundsOffsets: rangeBoundsOffsets,
+            unit: unit,
+            referenceValues: referenceValues,
+            accesibilityValues: accessibilityValues,
+            appearanceProperties: appearanceProperties
+        )
     }
 
     func buildRangeFilterInfoForBAPMarket(from filterData: FilterData) -> RangeFilterInfoType? {
-        let title = filterData.title
+        let name = filterData.title
         let lowValue: Int
         let highValue: Int
         let steps: Int
         let unit: String
+        let rangeBoundsOffsets: RangeFilterInfo.RangeBoundsOffsets
+        let referenceValues: RangeFilterInfo.ReferenceValues
+        let accessibilityValues: RangeFilterInfo.AccessibilityValues
+        let appearanceProperties: RangeFilterInfo.AppearenceProperties
 
         switch filterData.key {
         case .price:
             lowValue = 0
             highValue = 30000
-            steps = 300
             unit = "kr"
+            rangeBoundsOffsets = (0, 1000)
+            referenceValues = defaultReferencValuesForRange(withLowValue: lowValue, andHighValue: highValue)
+            steps = calculatedStepsForRange(withLowValue: lowValue, highValue: highValue, rangeBoundsOffsets: rangeBoundsOffsets, incrementedBy: 1000)
+            accessibilityValues = (accessibilitySteps: nil, accessibilityValueSuffix: nil)
+            appearanceProperties = (usesSmallNumberInputFont: false, displaysUnitInNumberInput: true, isCurrencyValueRange: true)
         default:
             return nil
         }
 
-        return RangeFilterInfo(name: title, lowValue: lowValue, highValue: highValue, steps: steps, unit: unit)
+        return RangeFilterInfo(
+            name: name,
+            lowValue: lowValue,
+            highValue: highValue,
+            steps: steps,
+            rangeBoundsOffsets: rangeBoundsOffsets,
+            unit: unit,
+            referenceValues: referenceValues,
+            accesibilityValues: accessibilityValues,
+            appearanceProperties: appearanceProperties
+        )
+    }
+
+    func defaultReferencValuesForRange(withLowValue lowValue: Int, andHighValue highValue: Int) -> [Int] {
+        return [lowValue, (lowValue + highValue / 2), highValue]
+    }
+
+    func calculatedStepsForRange(withLowValue lowValue: Int, highValue: Int, rangeBoundsOffsets: RangeFilterInfo.RangeBoundsOffsets, incrementedBy increments: Int) -> Int {
+        return RangeFilterInfoBuilder.calculatedStepsForRange(withLowValue: lowValue, highValue: highValue, rangeBoundsOffsets: rangeBoundsOffsets, incrementedBy: increments)
+    }
+}
+
+extension RangeFilterInfoBuilder {
+    static func calculatedStepsForRange(withLowValue lowValue: Int, highValue: Int, rangeBoundsOffsets: RangeFilterInfo.RangeBoundsOffsets, incrementedBy increments: Int) -> Int {
+        let lowerBound = lowValue - rangeBoundsOffsets.lowerBoundOffset
+        let upperBound = highValue + rangeBoundsOffsets.upperBoundOffset
+        let range = lowerBound ... upperBound
+        let steps = (range.upperBound - range.lowerBound) / increments
+
+        return steps
     }
 }
--- UnitTests/RangeFilterBuilderTests.swift
+++ UnitTests/RangeFilterBuilderTests.swift
@@ -30,16 +30,37 @@ class RangeFilterBuilderTests: BaseTestCase {
         }
         let fromYear = 1950
         let currentYear = Calendar.current.component(.year, from: Date())
+        let steps: Int?
+        if let filterInfo = yearRangefilterInfo {
+            steps = (currentYear + filterInfo.additionalUpperBoundOffset) - (fromYear - filterInfo.additonalLowerBoundOffset)
+        } else {
+            steps = nil
+        }
 
         // Then
         XCTAssertNotNil(yearRangeFilterData)
         XCTAssertNotNil(yearRangefilterInfo)
+        XCTAssertNotNil(steps)
 
         XCTAssertEqual(yearRangefilterInfo?.name, "Årsmodell")
         XCTAssertEqual(yearRangefilterInfo?.name, yearRangeFilterData?.title)
         XCTAssertEqual(yearRangefilterInfo?.lowValue, fromYear)
         XCTAssertEqual(yearRangefilterInfo?.highValue, currentYear)
-        XCTAssertEqual(yearRangefilterInfo?.steps, currentYear - fromYear)
+        XCTAssertEqual(yearRangefilterInfo?.steps, steps)
         XCTAssertEqual(yearRangefilterInfo?.unit, "år")
+    }
+
+    func testCalculatingStepsByIncrements() {
+        // Given
+        let lowValue = 1_000_000
+        let highValue = 10_000_000
+        let rangeBoundsOffsets = (100_000, 100_000)
+        let increments = 10_000
+
+        // When
+        let calculatedSteps = RangeFilterInfoBuilder.calculatedStepsForRange(withLowValue: lowValue, highValue: highValue, rangeBoundsOffsets: rangeBoundsOffsets, incrementedBy: increments)
+
+        // Then
+        XCTAssertEqual(calculatedSteps, 920)
     }
 }

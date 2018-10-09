c44ab319a9a4421ecf3b3235292b6aea65ee17ea
Jonas Olsson <jonas.olsson@finn.no>
2018-10-09 16:13:51 +0200

    Make PreferenceFilterListViewController work (select and show selection)

--- Sources/PreferenceFilter/PreferenceFilterListViewController.swift
+++ Sources/PreferenceFilter/PreferenceFilterListViewController.swift
@@ -21,22 +21,56 @@ public class PreferenceFilterListViewController: ListViewController, FilterConta
 
         self.preferenceInfo = preferenceInfo
         self.selectionDataSource = selectionDataSource
-        super.init(title: preferenceInfo.preferenceName, items: preferenceInfo.values, allowsMultipleSelection: preferenceInfo.isMultiSelect)
+        super.init(title: preferenceInfo.preferenceName, items: preferenceInfo.values, allowsMultipleSelection: preferenceInfo.isMultiSelect, listItemSelectionStateProvider: PreferenceSelectionStateProvider(filterInfo: preferenceInfo, selectionDataSource: selectionDataSource))
     }
 
     public required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+}
 
-    public override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        if preferenceInfo.isMultiSelect {
-            if let values = tableView.indexPathsForSelectedRows?.map({ preferenceInfo.values[$0.row].value }) {
-                selectionDataSource.setValue(values, for: preferenceInfo)
+private class PreferenceSelectionStateProvider: ListItemSelectionStateProvider {
+    private let filterInfo: PreferenceInfoType
+    let selectionDataSource: FilterSelectionDataSource
+    var isMultiSelectList: Bool {
+        return filterInfo.isMultiSelect
+    }
+
+    init(filterInfo: PreferenceInfoType, selectionDataSource: FilterSelectionDataSource) {
+        self.filterInfo = filterInfo
+        self.selectionDataSource = selectionDataSource
+    }
+
+    func toggleSelection(for listItem: ListItem) {
+        guard let item = listItem as? PreferenceValueType else {
+            return
+        }
+        let wasItemPreviouslySelected = isListItemSelected(item)
+        if isMultiSelectList {
+            if wasItemPreviouslySelected {
+                selectionDataSource.clearValue(item.value, for: filterInfo)
+            } else {
+                selectionDataSource.addValue(item.value, for: filterInfo)
             }
         } else {
-            if let value = tableView.indexPathForSelectedRow.map({ preferenceInfo.values[$0.row].value }) {
-                selectionDataSource.setValue([value], for: preferenceInfo)
+            selectionDataSource.clearAll(for: filterInfo)
+            if !wasItemPreviouslySelected {
+                selectionDataSource.setValue([item.value], for: filterInfo)
             }
         }
+    }
+
+    public func isListItemSelected(_ listItem: ListItem) -> Bool {
+        guard let item = listItem as? PreferenceValueType else {
+            return false
+        }
+        return isListSelectionFilterValueSelected(item)
+    }
+
+    private func isListSelectionFilterValueSelected(_ item: PreferenceValueType) -> Bool {
+        guard let currentSelection = selectionDataSource.value(for: filterInfo) else {
+            return false
+        }
+        return currentSelection.contains(item.value)
     }
 }

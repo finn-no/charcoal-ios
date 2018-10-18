ae15797e0d48252315b19e01dab03a87f3a625a6
Jonas Olsson <jonas.olsson@finn.no>
2018-10-18 12:06:35 +0200

    Localized "Bruk"

--- Sources/FilterViewController/FilterViewController.swift
+++ Sources/FilterViewController/FilterViewController.swift
@@ -35,7 +35,7 @@ public final class FilterViewController<ChildViewController: FilterContainerView
         let buttonView = FilterBottomButtonView()
         buttonView.translatesAutoresizingMaskIntoConstraints = false
         buttonView.delegate = self
-        buttonView.buttonTitle = "Bruk"
+        buttonView.buttonTitle = "apply-button-title".localized()
         return buttonView
     }()
 

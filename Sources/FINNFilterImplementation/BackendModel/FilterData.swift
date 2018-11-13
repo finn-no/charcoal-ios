30636702cb562182a4824c71d034bf1b8aac0d39
Jonas Olsson <jonas.olsson@finn.no>
2018-11-13 22:39:45 +0100

    Refactored. Group name DefaultFilterImplementation -> FINNFilterImplementation. Renamed and extracted FilterData types to separate files.

---
+++ Sources/FINNFilterImplementation/BackendModel/FilterData.swift
@@ -0,0 +1,39 @@
+//
+//  Copyright © FINN.no AS, Inc. All rights reserved.
+//
+
+struct FilterData: Decodable {
+    let title: String
+    let parameterName: String
+    let queries: [FilterDataQuery]
+    let isRange: Bool?
+
+    enum CodingKeys: String, CodingKey {
+        case title, parameterName = "name", queries, isRange = "range"
+    }
+
+    private init?(title: String, parameterName: String, isRange: Bool?, queries: [FilterDataQuery]?) {
+        self.title = title
+        self.parameterName = parameterName
+        self.isRange = isRange
+        self.queries = queries ?? []
+    }
+
+    static func decode(from dict: [AnyHashable: Any]?) -> FilterData? {
+        guard let dict = dict else {
+            return nil
+        }
+        guard let title = dict[CodingKeys.title.rawValue] as? String else {
+            return nil
+        }
+        guard let parameterName = dict[CodingKeys.parameterName.rawValue] as? String else {
+            return nil
+        }
+        let queriesArray = dict[CodingKeys.queries.rawValue] as? [[AnyHashable: Any]]
+        let queries = FilterDataQuery.decode(from: queriesArray)
+
+        let isRange = dict[CodingKeys.isRange.rawValue] as? Bool
+
+        return FilterData(title: title, parameterName: parameterName, isRange: isRange, queries: queries ?? [])
+    }
+}

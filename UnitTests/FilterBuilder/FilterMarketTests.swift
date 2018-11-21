37b0932b1f95afe8358cbf07f829d2651ea8a7c9
Jonas Olsson <osanoj@users.noreply.github.com>
2018-11-21 16:17:57 +0100

    Submarkets data (#89)
    * Added another car market to demo data
    
    * Expand FilterMarket configuration with subverticals
    
    * Refactored FilterMarket
    
    * Updated FilterMarkets to support multiple ids for same market (BAP)

---
+++ UnitTests/FilterBuilder/FilterMarketTests.swift
@@ -0,0 +1,25 @@
+//
+//  Copyright © FINN.no AS, Inc. All rights reserved.
+//
+
+@testable import Charcoal
+import XCTest
+
+class FilterMarketTests: NSObject {
+    func testFilterMarketAllCasesMustBeExhaustive() {
+        let results = verifyAllCasesIsExhaustive(filterMarket: .bap(.bap))
+        XCTAssertTrue(results, "If it compiles the test works...")
+    }
+}
+
+extension FilterMarketTests {
+    func verifyAllCasesIsExhaustive(filterMarket: FilterMarket) -> Bool {
+        switch filterMarket {
+        case .bap(.bap),
+             .realestate(.homes),
+             .car(.norway), .car(.abroad):
+            break
+        }
+        return true
+    }
+}

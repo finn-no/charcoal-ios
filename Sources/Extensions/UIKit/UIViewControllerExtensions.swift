56ef1727df1f0db602c843ba0dc9bd437c217cfb
Jonas Olsson <jonas.olsson@finn.no>
2018-11-21 14:22:47 +0100

    Added simple error view controller to show errors when loading filters failed

--- Sources/Extensions/UIKit/UIViewControllerExtensions.swift
+++ Sources/Extensions/UIKit/UIViewControllerExtensions.swift
@@ -21,3 +21,23 @@ extension UIViewController {
         }
     }
 }
+
+extension UIViewController {
+    func add(_ childViewController: UIViewController) {
+        guard childViewController.parent == nil else { return }
+
+        addChild(childViewController)
+        childViewController.view.frame = view.bounds
+        childViewController.view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        view.addSubview(childViewController.view)
+        childViewController.didMove(toParent: self)
+    }
+
+    func remove() {
+        guard parent != nil else { return }
+
+        willMove(toParent: nil)
+        removeFromParent()
+        view.removeFromSuperview()
+    }
+}

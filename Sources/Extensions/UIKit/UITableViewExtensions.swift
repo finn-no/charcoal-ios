ca5970f0713d062a6992fd828b64d3b93e982b60
Henrik Brustad <henrikbrustad@gmail.com>
2018-12-10 09:56:03 +0100

    adds finnivers kit framework

--- Sources/Extensions/UIKit/UITableViewExtensions.swift
+++ Sources/Extensions/UIKit/UITableViewExtensions.swift
@@ -4,14 +4,14 @@
 
 import UIKit
 
-extension UITableViewCell: Identifiable {}
-
-extension UITableView {
-    func register(_ cellClass: UITableViewCell.Type) {
-        register(cellClass.self, forCellReuseIdentifier: cellClass.reuseIdentifier)
-    }
-
-    func dequeue<T>(_ cellClass: T.Type, for indexPath: IndexPath) -> T where T: UITableViewCell {
-        return dequeueReusableCell(withIdentifier: cellClass.reuseIdentifier, for: indexPath) as! T
-    }
-}
+// extension UITableViewCell: Identifiable {}
+//
+// extension UITableView {
+//    func register(_ cellClass: UITableViewCell.Type) {
+//        register(cellClass.self, forCellReuseIdentifier: cellClass.reuseIdentifier)
+//    }
+//
+//    func dequeue<T>(_ cellClass: T.Type, for indexPath: IndexPath) -> T where T: UITableViewCell {
+//        return dequeueReusableCell(withIdentifier: cellClass.reuseIdentifier, for: indexPath) as! T
+//    }
+// }

32c6f4cd3f3429b2b8b54b33542300825f940493
Henrik Brustad <henrikbrustad@gmail.com>
2018-12-10 10:12:08 +0100

    adds hockey sdk to project root

--- Sources/Extensions/UIKit/UICollectionViewExtensions.swift
+++ Sources/Extensions/UIKit/UICollectionViewExtensions.swift
@@ -4,14 +4,14 @@
 
 import UIKit
 
-extension UICollectionViewCell: Identifiable {}
-
-extension UICollectionView {
-    func register(_ cellClass: UICollectionViewCell.Type) {
-        register(cellClass.self, forCellWithReuseIdentifier: cellClass.reuseIdentifier)
-    }
-
-    func dequeue<T>(_ cellClass: T.Type, for indexPath: IndexPath) -> T where T: UICollectionViewCell {
-        return dequeueReusableCell(withReuseIdentifier: T.reuseIdentifier, for: indexPath) as! T
-    }
-}
+// extension UICollectionViewCell: Identifiable {}
+//
+// extension UICollectionView {
+//    func register(_ cellClass: UICollectionViewCell.Type) {
+//        register(cellClass.self, forCellWithReuseIdentifier: cellClass.reuseIdentifier)
+//    }
+//
+//    func dequeue<T>(_ cellClass: T.Type, for indexPath: IndexPath) -> T where T: UICollectionViewCell {
+//        return dequeueReusableCell(withReuseIdentifier: T.reuseIdentifier, for: indexPath) as! T
+//    }
+// }

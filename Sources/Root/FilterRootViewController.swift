17e23b73c8fc0b67f6e0fb528472b7769b99dd64
Vadym Markov <markov.vadym@gmail.com>
2019-01-14 16:14:30 +0100

    Fix showResultsButtonView "jumping" when animating/sliding the bottom sheet (#108)
    * Fix jumping bottom button
    
    * Rename var to bottomSafeAreaInset

--- Sources/Root/FilterRootViewController.swift
+++ Sources/Root/FilterRootViewController.swift
@@ -134,6 +134,14 @@ public class FilterRootViewController: UIViewController {
         return cell
     }()
 
+    private var bottomSafeAreaInset: CGFloat {
+        if #available(iOS 11.0, *) {
+            return UIApplication.shared.delegate?.window??.safeAreaInsets.bottom ?? 0
+        } else {
+            return 0
+        }
+    }
+
     public init(title: String, navigator: RootFilterNavigator, selectionDataSource: FilterSelectionDataSource, filterSelectionTitleProvider: FilterSelectionTitleProvider, delegate: FilterRootViewControllerDelegate? = nil) {
         self.navigator = navigator
         self.selectionDataSource = selectionDataSource
@@ -177,7 +185,7 @@ private extension FilterRootViewController {
             showResultsButtonView.topAnchor.constraint(equalTo: tableView.bottomAnchor),
             showResultsButtonView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             showResultsButtonView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            showResultsButtonView.bottomAnchor.constraint(equalTo: bottomLayoutGuide.topAnchor),
+            showResultsButtonView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -bottomSafeAreaInset),
         ])
     }
 

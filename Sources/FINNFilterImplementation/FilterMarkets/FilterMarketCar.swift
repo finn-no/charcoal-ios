5e4b76a0370065629e67dc73eb8c2fb9ab9ff83a
Bastian Stien <bastian.stien@gmail.com>
2019-01-28 16:08:10 +0100

    Finish rangeFilters

--- Sources/FINNFilterImplementation/FilterMarkets/FilterMarketCar.swift
+++ Sources/FINNFilterImplementation/FilterMarkets/FilterMarketCar.swift
@@ -127,45 +127,135 @@ extension FilterMarketCar: FilterConfiguration {
         }
         switch filterKey {
         case .year:
-            lowValue = 1950
+            switch self {
+            case .norway, .abroad:
+                lowValue = 1950
+            case .mobileHome, .caravan:
+                lowValue = 1990
+            }
             highValue = Calendar.current.component(.year, from: Date())
             unit = "år"
             rangeBoundsOffsets = (hasLowerBoundOffset: true, hasUpperBoundOffset: true)
             increment = 1
             accessibilityValues = (stepIncrement: nil, valueSuffix: nil)
             appearanceProperties = (usesSmallNumberInputFont: false, displaysUnitInNumberInput: false, isCurrencyValueRange: false)
-        case .engineEffect:
-            lowValue = 0
-            highValue = 500
-            unit = "hk"
-            rangeBoundsOffsets = (hasLowerBoundOffset: false, hasUpperBoundOffset: true)
-            increment = 10
-            accessibilityValues = (stepIncrement: nil, valueSuffix: nil)
-            appearanceProperties = (usesSmallNumberInputFont: false, displaysUnitInNumberInput: true, isCurrencyValueRange: false)
         case .mileage:
+            switch self {
+            case .caravan:
+                highValue = 20000
+            default:
+                highValue = 200_000
+            }
             lowValue = 0
-            highValue = 200_000
             unit = "km"
             rangeBoundsOffsets = (hasLowerBoundOffset: false, hasUpperBoundOffset: true)
             increment = 1000
             accessibilityValues = (stepIncrement: nil, valueSuffix: nil)
             appearanceProperties = (usesSmallNumberInputFont: false, displaysUnitInNumberInput: true, isCurrencyValueRange: false)
-        case .numberOfSeats:
-            lowValue = 0
-            highValue = 10
-            unit = "seter"
-            rangeBoundsOffsets = (hasLowerBoundOffset: false, hasUpperBoundOffset: true)
-            increment = 1
-            accessibilityValues = (stepIncrement: nil, valueSuffix: nil)
-            appearanceProperties = (usesSmallNumberInputFont: false, displaysUnitInNumberInput: true, isCurrencyValueRange: false)
         case .price:
+            switch self {
+            case .norway, .abroad, .caravan:
+                highValue = 700_000
+            case .mobileHome:
+                highValue = 1_000_000
+            }
             lowValue = 0
-            highValue = 500_000
+            unit = "kr"
+            rangeBoundsOffsets = (hasLowerBoundOffset: false, hasUpperBoundOffset: true)
+            increment = 10000
+            accessibilityValues = (stepIncrement: nil, valueSuffix: nil)
+            appearanceProperties = (usesSmallNumberInputFont: true, displaysUnitInNumberInput: true, isCurrencyValueRange: true)
+        case .leasepriceInit:
+            lowValue = 0
+            highValue = 150_000
+            unit = "kr"
+            rangeBoundsOffsets = (hasLowerBoundOffset: false, hasUpperBoundOffset: true)
+            increment = 10000
+            accessibilityValues = (stepIncrement: nil, valueSuffix: nil)
+            appearanceProperties = (usesSmallNumberInputFont: false, displaysUnitInNumberInput: true, isCurrencyValueRange: true)
+        case .leasepriceMonth:
+            lowValue = 0
+            highValue = 10000
             unit = "kr"
             rangeBoundsOffsets = (hasLowerBoundOffset: false, hasUpperBoundOffset: true)
             increment = 1000
             accessibilityValues = (stepIncrement: nil, valueSuffix: nil)
             appearanceProperties = (usesSmallNumberInputFont: false, displaysUnitInNumberInput: true, isCurrencyValueRange: true)
+        case .engineEffect:
+            switch self {
+            case .norway, .abroad:
+                lowValue = 0
+                highValue = 500
+            default:
+                lowValue = 0
+                highValue = 300
+            }
+            unit = "hk"
+            rangeBoundsOffsets = (hasLowerBoundOffset: false, hasUpperBoundOffset: true)
+            increment = 10
+            accessibilityValues = (stepIncrement: nil, valueSuffix: nil)
+            appearanceProperties = (usesSmallNumberInputFont: false, displaysUnitInNumberInput: true, isCurrencyValueRange: false)
+        case .numberOfSeats:
+            switch self {
+            case .norway, .abroad:
+                highValue = 10
+            case .mobileHome:
+                highValue = 8
+            default:
+                return nil
+            }
+            lowValue = 0
+            unit = "seter"
+            rangeBoundsOffsets = (hasLowerBoundOffset: false, hasUpperBoundOffset: true)
+            increment = 1
+            accessibilityValues = (stepIncrement: nil, valueSuffix: nil)
+            appearanceProperties = (usesSmallNumberInputFont: false, displaysUnitInNumberInput: true, isCurrencyValueRange: false)
+        case .noOfSleepers:
+            lowValue = 0
+            highValue = 8
+            unit = "stk."
+            rangeBoundsOffsets = (hasLowerBoundOffset: false, hasUpperBoundOffset: true)
+            increment = 1
+            accessibilityValues = (stepIncrement: nil, valueSuffix: nil)
+            appearanceProperties = (usesSmallNumberInputFont: false, displaysUnitInNumberInput: true, isCurrencyValueRange: false)
+        case .length:
+            switch self {
+            case .mobileHome:
+                lowValue = 600
+            case .caravan:
+                lowValue = 500
+            default:
+                return nil
+            }
+            highValue = 950
+            unit = "cm"
+            rangeBoundsOffsets = (hasLowerBoundOffset: true, hasUpperBoundOffset: true)
+            increment = 50
+            accessibilityValues = (stepIncrement: nil, valueSuffix: nil)
+            appearanceProperties = (usesSmallNumberInputFont: false, displaysUnitInNumberInput: true, isCurrencyValueRange: false)
+        case .width:
+            lowValue = 200
+            highValue = 350
+            unit = "cm"
+            rangeBoundsOffsets = (hasLowerBoundOffset: true, hasUpperBoundOffset: true)
+            increment = 10
+            accessibilityValues = (stepIncrement: nil, valueSuffix: nil)
+            appearanceProperties = (usesSmallNumberInputFont: false, displaysUnitInNumberInput: true, isCurrencyValueRange: false)
+        case .weight:
+            switch self {
+            case .mobileHome:
+                lowValue = 3500
+                highValue = 7500
+            case .caravan:
+                lowValue = 1000
+                highValue = 3500
+            default: return nil
+            }
+            unit = "kg"
+            rangeBoundsOffsets = (hasLowerBoundOffset: true, hasUpperBoundOffset: true)
+            increment = 100
+            accessibilityValues = (stepIncrement: nil, valueSuffix: nil)
+            appearanceProperties = (usesSmallNumberInputFont: false, displaysUnitInNumberInput: true, isCurrencyValueRange: false)
         default:
             return nil
         }

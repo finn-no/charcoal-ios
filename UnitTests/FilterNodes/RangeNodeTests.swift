2510ddfab302387705161a820bf8ce3fa6b1a5a9
Henrik Brustad <henrikbrustad@gmail.com>
2019-02-08 13:59:56 +0100

    url items test for range node

--- UnitTests/FilterNodes/RangeNodeTests.swift
+++ UnitTests/FilterNodes/RangeNodeTests.swift
@@ -17,4 +17,26 @@ final class RangeNodeTests: XCTestCase {
         XCTAssertEqual(fromNode.name, "range_from")
         XCTAssertEqual(toNode.name, "range_to")
     }
+
+    func testUrlItems() {
+        let rangeNode = CCRangeFilterNode(title: "Range", name: "range")
+
+        let fromNode = rangeNode.child(at: CCRangeFilterNode.Index.from.rawValue)
+        fromNode.value = "value1"
+        fromNode.isSelected = true
+
+        let toNode = rangeNode.child(at: CCRangeFilterNode.Index.to.rawValue)
+        toNode.value = "value2"
+        toNode.isSelected = true
+
+        let urlItems = rangeNode.urlItems
+        XCTAssertEqual(urlItems.count, 2)
+        XCTAssertEqual(urlItems.first, "range_from=value1")
+        XCTAssertEqual(urlItems.last, "range_to=value2")
+
+        fromNode.isSelected = false
+        let urlItems2 = rangeNode.urlItems
+        XCTAssertEqual(urlItems2.count, 1)
+        XCTAssertEqual(urlItems2.first, "range_to=value2")
+    }
 }

839995c93d9d59a5ad868397617d8a8374797cfb
Vadym Markov <markov.vadym@gmail.com>
2019-02-15 14:36:21 +0100

    Filter view controller: disable swipe back gesture for ranges (#149)
    * Disable swipe back navigation
    
    * Disable swipe back for range view controller only

--- Sources/CCFilter/ViewControllers/CCFilterViewController.swift
+++ Sources/CCFilter/ViewControllers/CCFilterViewController.swift
@@ -56,7 +56,27 @@ public class CCFilterViewController: UINavigationController {
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+
+    // MARK: - Lifecycle
+
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        delegate = self
+    }
+
+    // MARK: - Private
+
+    private func updateLoading() {
+        if isLoading {
+            add(loadingViewController)
+            loadingViewController.viewWillAppear(false)
+        } else {
+            loadingViewController.remove()
+        }
+    }
 }
+
+// MARK: - CCRootFilterViewControllerDelegate
 
 extension CCFilterViewController: CCRootFilterViewControllerDelegate {
     func rootFilterViewController(_ viewController: CCRootFilterViewController, didSelectVerticalAt index: Int) {
@@ -108,13 +128,14 @@ extension CCFilterViewController: CCRootFilterViewControllerDelegate {
     }
 }
 
-private extension CCFilterViewController {
-    func updateLoading() {
-        if isLoading {
-            add(loadingViewController)
-            loadingViewController.viewWillAppear(false)
+// MARK: - UIGestureRecognizerDelegate
+
+extension CCFilterViewController: UINavigationControllerDelegate {
+    public func navigationController(_ navigationController: UINavigationController, didShow viewController: UIViewController, animated: Bool) {
+        if viewController is CCRangeFilterViewController {
+            interactivePopGestureRecognizer?.isEnabled = false
         } else {
-            loadingViewController.remove()
+            interactivePopGestureRecognizer?.isEnabled = true
         }
     }
 }

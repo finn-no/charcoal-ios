b4549fced6ebe176f07a2cdc74edd837dcff4912
Vadym Markov <markov.vadym@gmail.com>
2019-03-01 14:50:29 +0100

    Add support for external filters (#166)
    * Add external filter kind
    
    * Use cells from FinniversKit
    
    * Animate checkbox selection
    
    * Update FinniversKit
    
    * Refactor list fitler view controller
    
    * Test external filters
    
    * Remove separatorInset from filter cell
    
    * Rename pragma mark in ListFilterCellViewModel

--- Demo/ViewControllers/DemoViewsTableViewController.swift
+++ Demo/ViewControllers/DemoViewsTableViewController.swift
@@ -141,6 +141,10 @@ extension DemoViewsTableViewController: CharcoalViewControllerDelegate {
             viewController.config = config
         }
     }
+
+    func charcoalViewController(_ viewController: CharcoalViewController, didSelectExternalFilterWithKey key: String, value: String?) {
+        UIApplication.shared.openURL(URL(string: "https://finn.no")!)
+    }
 }
 
 extension DemoViewsTableViewController: FreeTextFilterDataSource, FreeTextFilterDelegate {

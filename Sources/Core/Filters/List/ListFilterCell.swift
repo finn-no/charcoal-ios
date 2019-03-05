dd240edf0d26491fa9b4986e7ebe2a3c5f6b4279
Vadym Markov <markov.vadym@gmail.com>
2019-03-05 12:53:00 +0100

    Set selection style for list filter cells

--- Sources/Core/Filters/List/ListFilterCell.swift
+++ Sources/Core/Filters/List/ListFilterCell.swift
@@ -24,16 +24,36 @@ final class ListFilterCell: CheckboxTableViewCell {
         fatalError("init(coder:) has not been implemented")
     }
 
+    // MARK: - Overrides
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        let isCheckboxHighlighted = checkbox.isHighlighted
+        super.setSelected(selected, animated: animated)
+        checkbox.isHighlighted = isCheckboxHighlighted
+    }
+
+    override func setHighlighted(_ highlighted: Bool, animated: Bool) {
+        let isCheckboxHighlighted = checkbox.isHighlighted
+        super.setHighlighted(highlighted, animated: animated)
+        checkbox.isHighlighted = isCheckboxHighlighted
+    }
+
     // MARK: - Setup
 
     func configure(with viewModel: ListFilterCellViewModel, animated: Bool) {
         super.configure(with: viewModel)
 
-        if viewModel.accessoryStyle == .external {
+        switch viewModel.accessoryStyle {
+        case .chevron:
+            selectionStyle = .default
+            chevronImageView.isHidden = true
+        case .external:
+            selectionStyle = .default
             chevronImageView.isHidden = false
             chevronImageView.image = UIImage(named: .webview).withRenderingMode(.alwaysTemplate)
             bringSubviewToFront(chevronImageView)
-        } else {
+        case .none:
+            selectionStyle = .none
             chevronImageView.isHidden = true
         }
 
@@ -43,8 +63,6 @@ final class ListFilterCell: CheckboxTableViewCell {
             checkbox.isHighlighted = false
             animateSelection(isSelected: viewModel.isSelected)
         }
-
-        selectionStyle = .none
     }
 
     private func setup() {
--- Sources/Core/Filters/Root/Views/RootFilterCell.swift
+++ Sources/Core/Filters/Root/Views/RootFilterCell.swift
@@ -60,7 +60,6 @@ final class RootFilterCell: UITableViewCell {
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         accessoryType = .disclosureIndicator
-        selectionStyle = .none
         setup()
     }
 

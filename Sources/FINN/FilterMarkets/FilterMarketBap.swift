ae524640f6a4486a240acfee86a092438bf54db7
Vadym Markov <markov.vadym@gmail.com>
2019-03-07 14:48:52 +0100

    Fix indentation in FilterMarketBap

--- Sources/FINN/FilterMarkets/FilterMarketBap.swift
+++ Sources/FINN/FilterMarkets/FilterMarketBap.swift
@@ -84,7 +84,7 @@ extension FilterMarketBap: FINNFilterConfiguration {
                         (range: 0 ..< 500, increment: 50),
                         (range: 500 ..< 1500, increment: 100),
                         (range: 1500 ..< 6000, increment: 500),
-                        ],
+                    ],
                     defaultIncrement: 1000
                 ),
                 hasLowerBoundOffset: false,

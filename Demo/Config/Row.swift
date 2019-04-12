b1bfcca86d6aba86ffc5cc7bbcfa602a9b7180cf
Henrik Brustad <henrikbrustad@gmail.com>
2019-04-12 09:37:30 +0200

    add search location, text editing delegate

--- Demo/Config/Row.swift
+++ Demo/Config/Row.swift
@@ -6,10 +6,15 @@ import FINNSetup
 import UIKit
 
 struct Row {
+
+    // MARK: - Internal properties
+
     let title: String
     let setup: Setup?
     let usingBottomSheet: Bool
     let type: UIViewController.Type
+
+    // MARK: - Init
 
     init(title: String, type: UIViewController.Type = CharcoalViewController.self, setup: Setup? = nil, usingBottomSheet: Bool = false) {
         self.title = title
--- Demo/ViewControllers/DemoTableViewController.swift
+++ Demo/ViewControllers/DemoTableViewController.swift
@@ -10,6 +10,8 @@ class DemoTableViewController: UIViewController {
     // MARK: - Private properties
 
     private let dataSource = DataSource()
+    private var bottomSheet: BottomSheet?
+    private let searchLocationDataSource = DemoSearchLocationDataSource()
 
     private lazy var tableView: UITableView = {
         let tableView = UITableView(frame: .zero, style: .grouped)
@@ -36,13 +38,26 @@ extension DemoTableViewController: UITableViewDelegate {
         var viewController = row.type.init()
 
         if let charcoalViewController = viewController as? CharcoalViewController {
+            charcoalViewController.textEditingDelegate = self
+            charcoalViewController.searchLocationDataSource = searchLocationDataSource
             charcoalViewController.filterContainer = row.setup?.filterContainer
         }
 
         if row.usingBottomSheet {
-            viewController = BottomSheet(rootViewController: viewController)
+            bottomSheet = BottomSheet(rootViewController: viewController)
+            viewController = bottomSheet!
         }
 
         present(viewController, animated: true)
+    }
+}
+
+extension DemoTableViewController: CharcoalViewControllerTextEditingDelegate {
+    func charcoalViewControllerWillBeginTextEditing(_ viewController: CharcoalViewController) {
+        bottomSheet?.state = .expanded
+    }
+
+    func charcoalViewControllerWillEndTextEditing(_ viewController: CharcoalViewController) {
+        bottomSheet?.state = .compact
     }
 }

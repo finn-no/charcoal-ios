1c3b02ea09e39b84a4cfc181c8778b0555c114f4
ag12 <amirg12@hotmail.no>
2019-04-15 13:17:06 +0200

    Removed some iOS 9 leftovers (#242)
    * Removed some iOS 9 leftovers
    
    * Addressed PR comments
    
    * Removed a guard

--- Sources/Charcoal/Haptics/FeedbackGenerator.swift
+++ Sources/Charcoal/Haptics/FeedbackGenerator.swift
@@ -12,18 +12,16 @@ final class FeedbackGenerator {
     }
 
     static func generate(_ feedback: Feedback) {
-        if #available(iOS 10.0, *) {
-            switch feedback {
-            case .selection:
-                let generator = UISelectionFeedbackGenerator()
-                generator.selectionChanged()
-            case .error:
-                let generator = UINotificationFeedbackGenerator()
-                generator.notificationOccurred(.error)
-            case .collision:
-                let generator = UIImpactFeedbackGenerator(style: .heavy)
-                generator.impactOccurred()
-            }
+        switch feedback {
+        case .selection:
+            let generator = UISelectionFeedbackGenerator()
+            generator.selectionChanged()
+        case .error:
+            let generator = UINotificationFeedbackGenerator()
+            generator.notificationOccurred(.error)
+        case .collision:
+            let generator = UIImpactFeedbackGenerator(style: .heavy)
+            generator.impactOccurred()
         }
     }
 }

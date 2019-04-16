93e5d2627bf4ebecdef893da5f467bd18cd80a61
Henrik Brustad <henrigbr@student.matnat.uio.no>
2019-04-16 09:57:02 +0200

    Change submarkets in demo (#245)
    * implement vertical change
    
    * simulate loading

--- Demo/DemoTableViewController.swift
+++ Demo/DemoTableViewController.swift
@@ -13,6 +13,8 @@ class DemoTableViewController: UIViewController {
     private var bottomSheet: BottomSheet?
     private let searchLocationDataSource = DemoSearchLocationDataSource()
 
+    private var currentRow: Row?
+
     private lazy var tableView: UITableView = {
         let tableView = UITableView(frame: .zero, style: .grouped)
         tableView.backgroundColor = .secondaryBlue
@@ -38,6 +40,7 @@ extension DemoTableViewController: UITableViewDelegate {
         var viewController = row.type.init()
 
         if let charcoalViewController = viewController as? CharcoalViewController {
+            charcoalViewController.selectionDelegate = self
             charcoalViewController.textEditingDelegate = self
             charcoalViewController.searchLocationDataSource = searchLocationDataSource
             charcoalViewController.filterContainer = row.setup?.filterContainer
@@ -48,8 +51,32 @@ extension DemoTableViewController: UITableViewDelegate {
             viewController = bottomSheet!
         }
 
+        currentRow = row
         present(viewController, animated: true)
     }
+}
+
+extension DemoTableViewController: CharcoalViewControllerSelectionDelegate {
+    func charcoalViewController(_ viewController: CharcoalViewController, didSelect vertical: Vertical) {
+        guard let vertical = vertical as? DemoVertical, let setup = currentRow?.setup else { return }
+
+        if let submarket = setup.markets.first(where: { $0.name == vertical.name }) {
+            setup.current = submarket
+            viewController.isLoading = true
+
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                viewController.filterContainer = setup.filterContainer
+                viewController.isLoading = false
+            }
+        }
+    }
+
+    func charcoalViewControllerDidPressShowResults(_ viewController: CharcoalViewController) {
+        bottomSheet?.state = .dismissed
+    }
+
+    func charcoalViewController(_ viewController: CharcoalViewController, didSelectExternalFilterWithKey key: String, value: String?) {}
+    func charcoalViewController(_ viewController: CharcoalViewController, didChangeSelection selection: [URLQueryItem], origin: SelectionChangeOrigin) {}
 }
 
 extension DemoTableViewController: CharcoalViewControllerTextEditingDelegate {

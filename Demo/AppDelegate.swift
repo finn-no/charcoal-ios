2ab7ace57aeb466f4d3b3b75f633f3882590139d
Jonas Olsson <osanoj@users.noreply.github.com>
2019-11-05 12:16:04 +0100

    Use AppCenter SDK for demo app (#306)
    * Added AppCenter SDK, removed HockeySDK
    
    * Added HUGE unused AppCenterAuth.framework to gitignore
    
    * Gitignore carthage mac and tvOs builds.
    
    * Updated Fastfile for AppCenter
    
    * Updated changelog and build number
    
    * Added fastlane appcenter plugin
    
    * Updated dependencies, remove Carthage build folders for Mac and tvOS

--- Demo/AppDelegate.swift
+++ Demo/AppDelegate.swift
@@ -2,9 +2,12 @@
 //  Copyright © FINN.no AS, Inc. All rights reserved.
 //
 
+import AppCenter
+import AppCenterAnalytics
+import AppCenterCrashes
+import AppCenterDistribute
 import Charcoal
 import FinniversKit
-import HockeySDK
 import UIKit
 
 @UIApplicationMain
@@ -12,13 +15,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        BITHockeyManager.shared().configure(withIdentifier: "dcb11108644344b5ae22b778ed0fcf9d")
-        BITHockeyManager.shared().crashManager.crashManagerStatus = .autoSend
-        BITHockeyManager.shared().logLevel = .warning
-        BITHockeyManager.shared().isFeedbackManagerDisabled = true
-        BITHockeyManager.shared().authenticator.identificationType = .anonymous
-        BITHockeyManager.shared().start()
-        BITHockeyManager.shared().authenticator.authenticateInstallation()
+        #if !DEBUG
+            MSAppCenter.start("dcb11108-6443-44b5-ae22-b778ed0fcf9d", withServices: [
+                MSCrashes.self,
+                MSDistribute.self,
+            ])
+            MSCrashes.setEnabled(true)
+            MSDistribute.setEnabled(true)
+            MSAppCenter.setLogLevel(.warning)
+        #endif
 
         if #available(iOS 13.0, *) {
             FinniversKit.userInterfaceStyleSupport = .dynamic
--- Demo/InfoPlist.h
+++ Demo/InfoPlist.h
@@ -1,1 +1,1 @@
-#define BUILD_VERSION      329
+#define BUILD_VERSION      340

094df9224b8ef9f52f689bfe09a0dd7337173fce
Bastian Stien <bastian.stien@gmail.com>
2021-02-10 15:46:00 +0100

    Add easier creation of price filter

--- Demo/Extensions/FilterExtensions.swift
+++ Demo/Extensions/FilterExtensions.swift
@@ -1,3 +1,7 @@
+//
+//  Copyright © FINN.no AS, Inc. All rights reserved.
+//
+
 import Charcoal
 
 extension Filter {
@@ -9,24 +13,43 @@ extension Filter {
         inline(title: "", key: "inline", subfilters: subfilters)
     }
 
-    static func createList(name: String, style: Filter.Style = .normal, count: Int = 10) -> Filter {
+    static func list(name: String, count: Int = 10, isContextFilter: Bool = false) -> Filter {
         Filter(
             kind: .standard,
             title: name,
             key: name,
             value: nil,
             numberOfResults: 0,
-            style: style,
+            style: isContextFilter ? .context : .normal,
             subfilters: createListItems(count: count)
         )
     }
 
-    private static func createListItems(count: Int, parentLevel: String? = nil) -> [Filter] {
+    static func price(isContextFilter: Bool = false) -> Filter {
+        range(title: "Pris", config: .priceConfiguration, isContextFilter: isContextFilter)
+    }
+
+    static func range(title: String, config: RangeFilterConfiguration, isContextFilter: Bool = false) -> Filter {
+        range(
+            title: title,
+            key: title,
+            lowValueKey: "\(title)_from",
+            highValueKey: "\(title)_to",
+            config: .priceConfiguration,
+            style: isContextFilter ? .context : .normal
+        )
+    }
+}
+
+// MARK: - Private extensions
+
+private extension Filter {
+    static func createListItems(count: Int, parentLevel: String? = nil) -> [Filter] {
         guard count > 0 else { return [] }
         return (1 ... count).map { Self.createListItem(id: $0, parentLevel: parentLevel) }
     }
 
-    private static func createListItem(id: Int, parentLevel: String?) -> Filter {
+    static func createListItem(id: Int, parentLevel: String?) -> Filter {
         let itemLevelString: String
         if let parentLevel = parentLevel {
             itemLevelString = "\(parentLevel)_\(id)"

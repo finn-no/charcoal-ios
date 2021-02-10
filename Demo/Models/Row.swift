8f11f3f9114865787f8fea0a4b9dcb90d3230d42
Bastian Stien <bastian.stien@gmail.com>
2021-02-10 15:43:45 +0100

    Use bottomSheet when presenting FilterContainer

--- Demo/Models/Row.swift
+++ Demo/Models/Row.swift
@@ -20,7 +20,7 @@ struct Row {
         self.usingBottomSheet = usingBottomSheet
     }
 
-    init(title: String, type: UIViewController.Type = CharcoalViewController.self, filterContainer: FilterContainer, usingBottomSheet: Bool = false) {
-        self.init(title: title, type: type, setup: Setup(filterContainer: filterContainer), usingBottomSheet: usingBottomSheet)
+    init(title: String, type: UIViewController.Type = CharcoalViewController.self, filterContainer: FilterContainer) {
+        self.init(title: title, type: type, setup: Setup(filterContainer: filterContainer), usingBottomSheet: true)
     }
 }

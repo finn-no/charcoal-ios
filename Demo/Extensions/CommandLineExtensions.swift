295cd5dd4e355cca6b6d475320e2b71f7eada3b9
Bastian Stien <bastian.stien@gmail.com>
2021-02-12 12:41:37 +0100

    Remove old Demo + its xcodeproj-file

--- Demo/Extensions/CommandLineExtensions.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Copyright © FINN.no AS, Inc. All rights reserved.
-//
-
-extension CommandLine {
-    static var isUITesting: Bool {
-        return arguments.contains("--uitesting")
-    }
-}

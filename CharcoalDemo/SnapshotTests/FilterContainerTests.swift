0db64d0d1d3b8b1d1ec4a9fadaf438afe1861075
Bastian Stien <bastian.stien@gmail.com>
2021-02-12 15:46:35 +0100

    Set diffTool to ksdiff

--- CharcoalDemo/SnapshotTests/FilterContainerTests.swift
+++ CharcoalDemo/SnapshotTests/FilterContainerTests.swift
@@ -1,11 +1,19 @@
 @testable import CharcoalDemo
 import Charcoal
+import SnapshotTesting
 import XCTest
 
 class FilterContainerTests: XCTestCase {
 
     private func snapshot(_ filterContainer: FilterContainer, delay: TimeInterval? = nil, record recording: Bool = false, testName: String = #function) {
         assertSnapshots(matching: filterContainer, delay: delay, record: recording, testName: testName)
+    }
+
+    override func setUp() {
+        super.setUp()
+
+        // used in SnapshotTesting error messages
+        diffTool = "ksdiff"
     }
 
     // MARK: - Tests

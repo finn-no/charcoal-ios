f52e5f113fe49622659276f69a31c26582a9eccb
Bastian Stien <bastian.stien@gmail.com>
2021-02-12 15:49:29 +0100

    Remove unused method

--- CharcoalDemo/SnapshotTests/XCTestCaseExtensions.swift
+++ CharcoalDemo/SnapshotTests/XCTestCaseExtensions.swift
@@ -27,29 +27,6 @@ extension XCTestCase {
         }
     }
 
-    func elementWithoutTests<T>(
-        for caseIterable: T.Type,
-        testMethodPrefix: String = "test"
-    ) -> [T] where T: CaseIterable, T: RawRepresentable, T.RawValue == String {
-        let testMethodPrefix = testMethodPrefix.lowercased()
-        var methodCount: UInt32 = 0
-
-        guard let methodList = class_copyMethodList(type(of: self), &methodCount) else {
-            return []
-        }
-
-        let testMethods = (0..<Int(methodCount))
-            .map({ index -> String in
-                let selName = sel_getName(method_getName(methodList[index]))
-                return String(cString: selName, encoding: .utf8)!.lowercased()
-            })
-            .filter({ $0.starts(with: testMethodPrefix) })
-
-        return caseIterable.allCases.filter({
-            !testMethods.contains("\(testMethodPrefix)\($0.rawValue)".lowercased())
-        })
-    }
-
     func assertSnapshots(
         matching filterContainer: FilterContainer,
         includeIPad: Bool = true,
@@ -87,7 +64,9 @@ extension XCTestCase {
     }
 }
 
-extension Snapshotting where Value == UIViewController, Format == UIImage {
+// MARK: - Private extension
+
+private extension Snapshotting where Value == UIViewController, Format == UIImage {
     static func image(
         on config: ViewImageConfig,
         interfaceStyle: SnapshotUserInterfaceStyle

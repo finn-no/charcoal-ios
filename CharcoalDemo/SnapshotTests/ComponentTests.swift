07e72988b3343ddc4c4576b79f4c0061c387e8c1
Bastian Stien <bastian.stien@gmail.com>
2021-02-15 10:57:15 +0100

    Add snapshot tests for components

---
+++ CharcoalDemo/SnapshotTests/ComponentTests.swift
@@ -0,0 +1,48 @@
+@testable import CharcoalDemo
+import Charcoal
+import SnapshotTesting
+import XCTest
+
+class ComponentTests: XCTestCase {
+
+    private func snapshot(_ viewController: UIViewController, delay: TimeInterval? = nil, record recording: Bool = false, testName: String = #function) {
+        assertSnapshots(
+            matching: viewController,
+            device: .iPhone,
+            delay: delay,
+            record: recording,
+            file: #file,
+            testName: testName
+        )
+
+        assertSnapshots(
+            matching: viewController,
+            device: .iPad,
+            delay: delay,
+            record: recording,
+            file: #file,
+            testName: testName
+        )
+    }
+
+    override func setUp() {
+        super.setUp()
+
+        // used in SnapshotTesting error messages
+        diffTool = "ksdiff"
+    }
+
+    // MARK: - Tests
+
+    func testInlineFilter() {
+        snapshot(InlineFilterDemoViewController())
+    }
+
+    func testRangeFilter() {
+        snapshot(RangeFilterDemoViewController())
+    }
+
+    func testStepperFilter() {
+        snapshot(StepperFilterDemoViewController())
+    }
+}

8ab71055d6393fb8222badd36e5d5433fe74b632
Bastian Stien <bastian.stien@gmail.com>
2021-03-04 10:24:13 +0100

    Bump FinniversKit to 76.x.x

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(name: "FinniversKit", url: "https://github.com/finn-no/FinniversKit.git", from: "71.0.0")
+        .package(name: "FinniversKit", url: "https://github.com/finn-no/FinniversKit.git", from: "76.0.0")
     ],
     targets: [
         .target(

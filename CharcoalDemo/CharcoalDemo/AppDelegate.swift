0d3c54c35e4dd17e343bd5a4bfb18366396b6866
Nina Graneggen <ninagraneggen@gmail.com>
2022-06-28 09:35:12 +0200

    iOS 13 is minimum target in FinniversKit - so remove userInterfaceStyleSupport and iOS 13 check

--- CharcoalDemo/CharcoalDemo/AppDelegate.swift
+++ CharcoalDemo/CharcoalDemo/AppDelegate.swift
@@ -4,32 +4,21 @@ import FinniversKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        if #available(iOS 13.0, *) {
-            FinniversKit.Config.userInterfaceStyleSupport = .dynamic
+        let navigationBarAppearance = UINavigationBar.appearance()
+        navigationBarAppearance.isTranslucent = false
+        navigationBarAppearance.tintColor = .textAction
 
-            let navigationBarAppearance = UINavigationBar.appearance()
-            navigationBarAppearance.isTranslucent = false
-            navigationBarAppearance.tintColor = .textAction
+        let appearance = UINavigationBarAppearance()
+        appearance.backgroundColor = Theme.mainBackground
 
-            let appearance = UINavigationBarAppearance()
-            appearance.backgroundColor = Theme.mainBackground
+        appearance.titleTextAttributes = [
+            .foregroundColor: UIColor.textPrimary,
+            .font: UIFont.bodyStrong
+        ]
 
-            appearance.titleTextAttributes = [
-                .foregroundColor: UIColor.textPrimary,
-                .font: UIFont.bodyStrong,
-            ]
-
-            navigationBarAppearance.standardAppearance = appearance
-            navigationBarAppearance.scrollEdgeAppearance = appearance
-            navigationBarAppearance.compactAppearance = appearance
-        } else {
-            UINavigationBar.appearance().barTintColor = Theme.mainBackground
-            UINavigationBar.appearance().tintColor = .btnPrimary
-            UINavigationBar.appearance().isTranslucent = false
-            UINavigationBar.appearance().titleTextAttributes = [.font: UIFont.bodyStrong, .foregroundColor: UIColor.textPrimary]
-            UIBarButtonItem.appearance().setTitleTextAttributes([.font: UIFont.body, .foregroundColor: UIColor.textPrimary], for: .normal)
-        }
-
+        navigationBarAppearance.standardAppearance = appearance
+        navigationBarAppearance.scrollEdgeAppearance = appearance
+        navigationBarAppearance.compactAppearance = appearance
         return true
     }
 

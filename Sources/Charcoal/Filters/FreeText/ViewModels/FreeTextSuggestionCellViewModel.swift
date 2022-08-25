5f9e60e1052881fee29aa1ccae2d30f53076c5d4
Andrii Momot <andrii.momot@schibsted.com>
2022-08-25 16:05:09 +0200

    Conform to changes in IconTitleTableViewCellViewModel protocol

--- Sources/Charcoal/Filters/FreeText/ViewModels/FreeTextSuggestionCellViewModel.swift
+++ Sources/Charcoal/Filters/FreeText/ViewModels/FreeTextSuggestionCellViewModel.swift
@@ -9,6 +9,7 @@ struct FreeTextSuggestionCellViewModel: IconTitleTableViewCellViewModel {
     let icon: UIImage? = UIImage(named: .searchSmall)
     let iconTintColor: UIColor? = nil
     let hasChevron = false
+    let externalIcon: UIImage? = nil
     let subtitle: String? = nil
     let detailText: String? = nil
 }

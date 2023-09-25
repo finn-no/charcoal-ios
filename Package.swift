bd011fe82e1bbf05074df212042c6e956c661871
Bjørn Olav Ruud <bjorn.olav.ruud@schibsted.com>
2023-09-25 14:28:38 +0200

    Project and package settings updates.

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.8
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "Charcoal",
     defaultLocalization: "nb",
-    platforms: [.iOS(.v14)],
+    platforms: [.iOS(.v15)],
     products: [
         .library(
             name: "Charcoal",
@@ -14,7 +14,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(name: "FinniversKit", url: "https://github.com/finn-no/FinniversKit.git", "117.0.0"..."999.0.0")
+        .package(url: "https://github.com/finn-no/FinniversKit.git", "117.0.0"..."999.0.0")
     ],
     targets: [
         .target(

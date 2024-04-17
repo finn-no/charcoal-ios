5c011803c4dffbb391b29b7a8d906cf36f35d271
Sina <rabie_sina@yahoo.com>
2024-04-17 13:35:35 +0200

    Dismiss view if freeTextOnly option is selected

--- Sources/Charcoal/Filters/FreeText/FreeTextFilterViewController.swift
+++ Sources/Charcoal/Filters/FreeText/FreeTextFilterViewController.swift
@@ -31,6 +31,10 @@ public class FreeTextFilterViewController: ScrollViewController {
 
     weak var delegate: FreeTextFilterViewControllerDelegate?
 
+    // MARK: - Internal Properties
+    
+    var isFreeTextOnlySelected = false
+
     // MARK: - Private Properties
 
     private var didClearText = false
@@ -61,10 +65,11 @@ public class FreeTextFilterViewController: ScrollViewController {
 
     // MARK: - Init
 
-    init(filter: Filter, selectionStore: FilterSelectionStore, notificationCenter: NotificationCenter = .default) {
+    init(filter: Filter, selectionStore: FilterSelectionStore, notificationCenter: NotificationCenter = .default, isFreeTextOnlySelected: Bool = false) {
         self.filter = filter
         self.selectionStore = selectionStore
         self.notificationCenter = notificationCenter
+        self.isFreeTextOnlySelected = isFreeTextOnlySelected
         super.init(nibName: nil, bundle: nil)
     }
 
@@ -223,11 +228,21 @@ extension FreeTextFilterViewController: UISearchBarDelegate {
 
 private extension FreeTextFilterViewController {
     func returnToSuperView() {
+        guard !isFreeTextOnlySelected else {
+            dismissView()
+            return
+        }
+        
         if view.superview != nil {
             searchBar.endEditing(false)
             searchBar.setShowsCancelButton(false, animated: false)
             delegate?.freeTextFilterViewControllerWillEndEditing(self)
         }
+    }
+
+    func dismissView() {
+        dismiss(animated: true)
+        isFreeTextOnlySelected = false
     }
 
     func setup() {

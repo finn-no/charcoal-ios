4289a6218836357890d012c98b2201908ff6afdd
Sina <rabie_sina@yahoo.com>
2024-04-17 13:32:26 +0200

    Handle isFreeTextOnlySelected in RootFilterViewController

--- Sources/Charcoal/Filters/Root/RootFilterViewController.swift
+++ Sources/Charcoal/Filters/Root/RootFilterViewController.swift
@@ -28,6 +28,7 @@ final class RootFilterViewController: FilterViewController {
         didSet { freeTextFilterViewController?.filterDataSource = freeTextFilterDataSource }
     }
 
+    var isFreeTextOnlySelected: Bool = false
     var focusOnFreeTextFilterOnNextAppearance: Bool = false
 
     // MARK: - Private properties
@@ -113,6 +114,11 @@ final class RootFilterViewController: FilterViewController {
         if focusOnFreeTextFilterOnNextAppearance {
             freeTextFilterViewController?.searchBar.becomeFirstResponder()
             focusOnFreeTextFilterOnNextAppearance = false
+        }
+        
+        if isFreeTextOnlySelected {
+            freeTextFilterViewController?.isFreeTextOnlySelected = true
+            isFreeTextOnlySelected = false
         }
     }
 
@@ -469,7 +475,7 @@ private extension RootFilterViewController {
         guard let freeTextFilter = filterContainer.freeTextFilter else { return }
 
         if freeTextFilterViewController == nil {
-            let freeTextFilterViewController = FreeTextFilterViewController(filter: freeTextFilter, selectionStore: selectionStore)
+            let freeTextFilterViewController = FreeTextFilterViewController(filter: freeTextFilter, selectionStore: selectionStore, isFreeTextOnlySelected: isFreeTextOnlySelected)
             self.freeTextFilterViewController = freeTextFilterViewController
             freeTextFilterViewController.delegate = self
             freeTextFilterViewController.filterDelegate = freeTextFilterDelegate

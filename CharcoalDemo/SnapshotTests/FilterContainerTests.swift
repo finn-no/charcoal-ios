a7bda82c0dc07d4551ceb085dda3a0a779bb7471
Nina Røsdal Graneggen <ninagraneggen@gmail.com>
2024-07-11 12:14:30 +0200

    Snapshot of reload button

--- CharcoalDemo/SnapshotTests/FilterContainerTests.swift
+++ CharcoalDemo/SnapshotTests/FilterContainerTests.swift
@@ -8,10 +8,18 @@ class FilterContainerTests: XCTestCase {
     private func snapshot(
         _ filterContainer: FilterContainer,
         verticals: [DemoVertical] = .multiple,
+        showReloadVerticalsButton: Bool = false,
         record recording: Bool = false,
         testName: String = #function
     ) {
-        assertSnapshots(matching: filterContainer, delay: 0.1, record: recording, testName: testName)
+        assertSnapshots(
+            matching: filterContainer,
+            verticals: verticals,
+            showReloadVerticalsButton: showReloadVerticalsButton,
+            delay: 0.1,
+            record: recording,
+            testName: testName
+        )
     }
 
     override func setUp() {
@@ -28,6 +36,9 @@ class FilterContainerTests: XCTestCase {
     }
     func testMultipleVerticals() {
         snapshot(.standard, verticals: .multiple)
+    }
+    func testFailedToLoadVerticals() {
+        snapshot(.standard, verticals: .none, showReloadVerticalsButton: true)
     }
     func testContextFilter() {
         snapshot(.contextFilter)

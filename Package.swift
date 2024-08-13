892ac9ffe25448b5e6e66a77de0e6052b14de7dd
Mohsen <mohsen.hosseinpour@schibsted.com>
2024-08-13 19:41:04 +0200

    Remove color provider

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/finn-no/FinniversKit.git", "143.1.0"..."999.0.0")
+        .package(url: "https://github.com/finn-no/FinniversKit.git", branch: "remove-color-provider")
     ],
     targets: [
         .target(
